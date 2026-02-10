ff565240d256c284d1c49a7df8ad579cb4a51816
Silvia Teletor <teletorsilvia03@gmail.com>
2026-02-10 17:00:17 +0100

    Modify the navigation menu from links to buttons and add a contact form

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function goMenu() {
         document.getElementById("menu").scrollIntoView({ behavior: "smooth" });
     }
+
+    function irInicio(){
+        document.getElementById("home").scrollIntoView({behavior:"auto"})
+    }
+
+    function sobreMi(){
+        document.getElementById("myExperience").scrollIntoView({behavior:"smooth"})
+    }
+
+    function contacto() {
+        document.getElementById("contactame").scrollIntoView({behavior:"smooth"})
+    }
